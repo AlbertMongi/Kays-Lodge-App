82c4030cad9bef059c38f0b401b1df630df66555
Albert Mongi <mongialbert66@gmail.com>
2024-07-17 02:08:45 -0700

    Added or updated Procfile for Heroku deployment

--- kays_lodge/settings.py
+++ kays_lodge/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_on_heroku 
+import django_heroku 
 import dj_database_url 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,4 +170,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 CSRF_TRUSTED_ORIGINS = []
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
